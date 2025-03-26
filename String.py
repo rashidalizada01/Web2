823d8ca5693138dba6387f52d91353dd25f09ab2
rashidalizada01 <rashidalizada01@icloud.com>
2025-03-26 18:40:35 +0400

    dvfd

---
+++ String.py
@@ -0,0 +1,39 @@
+# name = 'John'
+# number = '+994502030510'
+#print(number.index('4',5,9))
+#print(number.count('4'))
+#print(number.strip())
+#print(number.split('&'))
+#url = 'https://linkedin.com?name=Rashid;age=18;level=senior'
+#print(url.split('?'))
+# email = 'rashidalizade01@icloud.com'
+# print(number.startswith('994'))
+# print(number.startswith('+994'))
+# print(email.endswith('icloud.com'))
+# print(len(number))
+# print(number.removeprefix('+'))
+# print(number)
+# if (number.startswith('+')):
+#     number = number.removeprefix('+')
+#     print(number)
+# test = 'abba'
+# if (test[::-1]==test):
+    # print("kjsfhdv")
+# print(number[::-1z])
+# print(number.replace('5', '6',1,))
+# number = number.strip()
+# number = number.replace(" ",'')
+# if (number.startswith('+994')):
+#     number = number.removeprefix('+994')
+#     print(number)
+# if (number.startswith('994')):
+#     number = number.removeprefix('994')
+#     print(number)
+# if (number.startswith('0')):
+#     number = number.removeprefix('0')
+#     print(number)
+# if (number[0:2] == '55'):
+#     print('This number belong to bakcel provider')
+name = 'John'
+number = '994552030510'
+print(''.join(number))
